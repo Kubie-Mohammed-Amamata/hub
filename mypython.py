8b77c41635d2afe393560311f4f4fa82523ce1d4
Kubie-Mohammed-Amamata <kubiem7@gmail.com>
2020-07-03 12:19:32 +0100

    initial commit

---
+++ mypython.py
@@ -0,0 +1,12 @@
+
+myname ="Kubie Mohammed Amamata"
+mynumber =100
+mybool =true
+
+
+def full_name(first, last):
+    print(first +" " +last)
+
+
+    full_name("Amamata" "Kubie Mohammed")
+    #comments in python
